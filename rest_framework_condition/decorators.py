7fe7e94a3093b4ae394d223955970a0ba354e23b
Terence D. Honles <terence@honles.com>
2020-07-31 13:48:58 -0700

    update decorator to not create decorated functions per request
    While the existing implementation is simple it: creates a decorated
    function per request, calls it, and then will discard it. Since the
    functionality is straightforward and also encapsulated fairly well in
    Django, this PR modifies the decorator to copy the original Django
    decorator, and calls the same helper functions where possible.
    
    Because the functionality is no longer simply wrapping Django's internal
    implementation, but is still mostly the same, there have been tests
    added to compare the requests generated by Django vs requests generated
    by Django Request Framework/this library.
    
    Finally, in order to be less confusing to library writers the call
    signature of `etag_func` and `last_modified_func` has been updated to
    match Django Request Framework and will pass a DRF Request object
    instead of Django's native Request object::
    
      # from the original signature
      def original_etag_func(wsgi_request, *args, **kwargs): pass
    
      # to the updated signature
      def updated_etag_func(drf_self, drf_request, *args, **kwargs): pass
    
    In order to ease the transition between the two signatures, there has
    been a flag "use_self" added to the decorators, and a warning will be
    emitted if use_self has not been set to True.

--- rest_framework_condition/decorators.py
+++ rest_framework_condition/decorators.py
@@ -1,36 +1,90 @@
 import functools
-from django.views.decorators.http import condition as django_condition
+import warnings
+
+from calendar import timegm
+
+from django.utils.cache import get_conditional_response
+from django.utils.http import http_date, quote_etag
 
 
-def condition(etag_func=None, last_modified_func=None):
+def condition(etag_func=None, last_modified_func=None, use_self=False):
     """
-    Decorator to support conditional retrieval (or change)
-    for a Django Rest Framework's ViewSet.
+    Decorator to support conditional retrieval (or change) for a Django Rest
+    Framework's ViewSet.
 
-    It calls Django's original decorator but pass correct request object to it.
-    Django's original decorator doesn't work with DRF request object.
+    This decorator emulates Django's original decorator by wrapping the
+    underlying functionality where possible but handles the Django Rest
+    Framework request object.
+
+    See: django.views.decorators.http.condition
     """
+
+    if not use_self:
+        warnings.warn(
+            'The etag_func and last_modified_func should accept a "self" '
+            'argument which matches how Django Rest Framework calls '
+            'view/viewset methods.\n\n'
+            'After updating the handlers pass "use_self" to the condition '
+            'decorator to enable the future functionality and silence this '
+            'warning.',
+            DeprecationWarning)
+
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(obj_self, request, *args, **kwargs):
-            drf_request = request
-            wsgi_request = request._request
+        def wrapper(self, request, *args, **kwargs):
+            if etag_func:
+                if use_self:
+                    etag = etag_func(self, request, *args, **kwargs)
+                else:
+                    etag = etag_func(request, *args, **kwargs)
 
-            def patched_viewset_method(*_args, **_kwargs):
-                """Call original viewset method with correct type of request"""
-                return func(obj_self, drf_request, *args, **kwargs)
+                # The value from etag_func() could be quoted or unquoted.
+                if etag:
+                    etag = quote_etag(etag)
+            else:
+                etag = None
 
-            django_decorator = django_condition(etag_func, last_modified_func)
-            decorated_viewset_method = django_decorator(patched_viewset_method)
-            return decorated_viewset_method(wsgi_request, *args, **kwargs)
+            if last_modified_func:
+                if use_self:
+                    last_modified = last_modified_func(
+                        self, request, *args, **kwargs)
+                else:
+                    last_modified = last_modified_func(
+                        request, *args, **kwargs)
+
+                if last_modified:
+                    last_modified = timegm(last_modified.utctimetuple())
+            else:
+                last_modified = None
+
+            # pass the wrapped WSGI request for Django
+            response = get_conditional_response(
+                request._request,
+                etag=etag,
+                last_modified=last_modified,
+            )
+
+            if response is None:
+                response = func(self, request, *args, **kwargs)
+
+            # Set relevant headers on the response if they don't already exist
+            # and if the request method is safe.
+            if request.method in ('GET', 'HEAD'):
+                if last_modified and not response.has_header('Last-Modified'):
+                    response['Last-Modified'] = http_date(last_modified)
+                if etag:
+                    response.setdefault('ETag', etag)
+
+            return response
+
         return wrapper
     return decorator
 
 
 # Shortcut decorators for common cases based on ETag or Last-Modified only
-def etag(etag_func):
-    return condition(etag_func=etag_func)
+def etag(etag_func, use_self=False):
+    return condition(etag_func=etag_func, use_self=use_self)
 
 
-def last_modified(last_modified_func):
-    return condition(last_modified_func=last_modified_func)
+def last_modified(last_modified_func, use_self=False):
+    return condition(last_modified_func=last_modified_func, use_self=use_self)
