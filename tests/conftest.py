22e18b8c7637d1985ae4afbf5bbc852e653affb8
jozo <hi@jozo.io>
2021-05-15 21:52:36 +0200

    Fix tests in Django 3.2

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ def pytest_configure():
         DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3',
                                'NAME': ':memory:'}},
         ROOT_URLCONF='tests.urls',
+        SECRET_KEY="super-secret",
         INSTALLED_APPS=[
             'django.contrib.auth',
             'django.contrib.contenttypes',
